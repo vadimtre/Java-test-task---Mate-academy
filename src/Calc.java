b8b76c0a7f311a0a2bc8a9bab0f78001f05e0329
vadimtre <vadimtre@gmail.com>
2019-02-16 23:53:48 +0200

    work variant

--- src/Calc.java
+++ src/Calc.java
@@ -1,40 +1,99 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Calc {
 	static Scanner sc=new Scanner(System.in);
+	static int first, second;
+	static String operation;
+	static boolean isEnteredRoman;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		first = enterNumber("first");
+		second = enterNumber("second");
+		operation = enterOperation();
+		System.out.println(first);
+		System.out.println(second);
+		System.out.println(operation);
 		
-		
-		int first = enterNumber("first");
-		int second = enterNumber("second");
-		
-		
-		
+		makeOperation();
 	}
 	
-	static int enterNumber(String whatIsNumber) {
-		System.out.printf("Enter the %s number: ", whatIsNumber);
-		//System.out.printf("%s%snter number from 1 to 10 where you want to go .", player, beginSentence);
-		do {
-			int first = sc.nextInt();
+	static int enterNumber(String whatNumber) {
+		do {System.out.printf("Enter a %s number: ", whatNumber);
+			 if(sc.hasNextInt()) { // возвращает истинну если с потока ввода можно считать целое число
+				 int enteredNumber =  sc.nextInt(); // считывает целое число с потока ввода и сохраняем в переменную
+				 return enteredNumber;
+		     }else {
+	        	String enteredString =  sc.next();
+	        	if (isRomanNumeral(enteredString)) {
+	        		if (whatNumber.equals("second") && !isEnteredRoman) {
+	        			System.out.println("Увага! Неможливо проводити математичні операції арабським числом та римським одночасно . Введіть арабське число !!!");
+					} else {
+	        		isEnteredRoman= true;
+					return convertRomanNumeralsToArabic(enteredString);
+					}
+				}else{
+					System.out.println("Вы ввели не римське число");
+				};
+		     }	
 		} while (true);
+	}
+	
+	static String enterOperation() {
+		System.out.print("Enter a operation(+,-,/,*): ");
+		
+		do {
+			String enteredOperation = sc.next();
+			return enteredOperation;
+		} while (true);
+	}
+	
+	static boolean isRomanNumeral(String mayBeRomanNumeral) {
+		String RomanNumerals[]= {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+		boolean isRoman = false;
+		
+		for (String string : RomanNumerals) {
+			if (mayBeRomanNumeral.equals(string) ) {
+				isRoman= true;
+				break;
+			}
+		}
+		return isRoman;
 	}
 	
 	static int convertRomanNumeralsToArabic(String romanNumeral) {
 		int ArabicNumerals[]= {0,1,2,3,4,5,6,7,8,9,10};
 		String RomanNumerals[]= {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+		int convertToArabic =0;
 		
-		for (String string : RomanNumerals) {
-			if (romanNumeral == string) {
-				
+		for (int i = 0; i < RomanNumerals.length; i++) {
+			if (romanNumeral.equals(RomanNumerals[i])) {
+				convertToArabic = ArabicNumerals[++i];
+				break;
 			}
 		}
-		return 0;
+		
+		return convertToArabic;	
 	}
 	
-	void multiply() {
+	static void makeOperation() {
+		int result=0 ;
 		
+		switch (operation) {
+		case "+":
+			result= first + second;
+			break;
+		case "-":
+			result= first - second;
+			break;
+		case "*":
+			result= first * second;
+			break;
+		case "/":
+			result= first / second;
+			break;
+		}
+		System.out.println("Result "+first + operation+ second+ " is " + result);
 	}
 
 }
